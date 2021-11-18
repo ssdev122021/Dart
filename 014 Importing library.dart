6e1a5b5c47dfbec3e01368704cf25289cb11a1fd
ssdev122021 <88177323+ssdev122021@users.noreply.github.com>
2021-11-18 13:21:10 +0630

    Create 014 Importing library.dart

---
+++ 014 Importing library.dart
@@ -0,0 +1,18 @@
+// The Dart provides the import keyword, which is used to make the library available in the current file
+
+// import installed Library
+// import 'package:xml/xml.dart' as xml;
+
+// Name Alias of Library
+import 'package:xml/xml.dart' as xml;
+
+// Importing built-in library  
+// import 'dart:math';   
+    
+//import coustom library
+import 'bscmath.dart';
+
+void main() {
+  print(sum(2, 2));
+  print(sub(4, 2));
+}
