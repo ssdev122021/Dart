63eba8d7edd30fbb0d46f937e19d2ecc618ec12c
ssdev122021 <88177323+ssdev122021@users.noreply.github.com>
2021-11-18 14:09:01 +0630

    Update 014 Importing library.dart

--- 014 Importing library.dart
+++ 014 Importing library.dart
@@ -4,13 +4,16 @@
 // import 'package:xml/xml.dart' as xml;
 
 // Name Alias of Library
-import 'package:xml/xml.dart' as xml;
+//import 'package:xml/xml.dart' as xml;
 
 // Importing built-in library  
 // import 'dart:math';   
     
 //import coustom library
 import 'bscmath.dart';
+
+// import coustom library from folder
+//import 'lib/bscmath.dart';
 
 void main() {
   print(sum(2, 2));
