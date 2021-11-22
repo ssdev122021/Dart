463418e525734c56b4da1279078c373c7b2fead4
ssdev122021 <88177323+ssdev122021@users.noreply.github.com>
2021-11-22 23:21:37 +0630

    Update 002 Variables and Data Type.dart

--- 002 Variables and Data Type.dart
+++ 002 Variables and Data Type.dart
@@ -1,12 +1,22 @@
 void main(){
+    
+   // Integer
    int age = 23;
+   //double
    double height = 5.4;
+
+   //String
    String name = 'Shine Shine';
+   String work = 'Dev';
+   // boolean
    bool isSingle = true;
+   // list
    var lst = [1,2,2,0,true,'Shine Shine'];
+   //map
    var post = {'title' : 'post one','desc' : 'post one','like' :100, 'publish' : true};
+   //Runes
    var heart_symbol = '\u2665';  
-   var laugh_symbol = '\u{1f600}';  
+   dynamic laugh_symbol = '\u{1f600}';  
 
    print(name);
    print(age);
@@ -16,6 +26,14 @@ void main(){
    print(post);
    print(heart_symbol);  
    print(laugh_symbol);  
+   
+   // concatenating
+
+   // interpolation String with String
+   print("Name : " + name + " Work : " + work );
+    
+   // interpolation String with integer
+   print("Name : ${name} and age :  ${age}");
 }
 
 // Built-in Data types  Number
