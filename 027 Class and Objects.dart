7334ca7f393048fd7711c3c7d8d4eda246a98378
ssdev122021 <88177323+ssdev122021@users.noreply.github.com>
2021-11-28 22:46:08 +0630

    Create 027 Class and Objects.dart

---
+++ 027 Class and Objects.dart
@@ -0,0 +1,16 @@
+// Dart classes are defined as the blueprint of the associated objects.
+// A Class is a user-defined data type that describes the characteristics and behavior of it. 
+
+// An object is a real-time representation of any entity. 
+// A object is an Instance of a class
+
+
+class Car{
+  
+}
+
+void main(){
+    Car car1 = new Car();
+    var car2 =  Car();
+    print(car1);
+}
